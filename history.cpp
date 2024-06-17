dc31062632be92a1d33e574c1c32178235a6d7aa
Yuan-Jialin <63775974+Yuan-Jialin@users.noreply.github.com>
2024-06-17 15:43:29 +0800

    Add files via upload

---
+++ history.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 57259 on 2024/6/17.
+//
+#include<iostream>
+#include "vector"
+#include "string"
+
+using namespace std;
+
+vector<string>v;
+
+void add(string command){
+    v.push_back(command);
+};
